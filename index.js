0897a94abbeaf4d1e38478fd49c065de15f2f634
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-27 08:02:47 -0500

    function review

---
+++ index.js
@@ -0,0 +1,83 @@
+//document.getElementById("count-el").innerText = 5
+
+//cmd+k+c
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+// 1. Create two variables, myAge and humanDogRatio
+
+// let myAge = 38
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+// let count = 5
+
+//count + 1
+// count = count + 1
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// bonusPoints = bonusPoints - 75
+// bonusPoints = bonusPoints + 45
+
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// Setting up the the race 🏎 🏎 🏎
+
+// countdown()
+
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+// Get ready for a new race 🏎 🏎 🏎
+
+// countdown()
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+// function logNumber() {
+//     console.log(42)
+// }
+
+// logNumber()
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+
+function sum() {
+    let sum = lap1 + lap2 + lap3
+    console.log(sum)
+}
+
+sum()
+
+
+// intialize the count as 0
+let count = 0
+
+// listen for clicks on the increment button
+function increment() {
+
+}
+// increment the count variable when the button is clicked
+// change the count-el in the HTML
